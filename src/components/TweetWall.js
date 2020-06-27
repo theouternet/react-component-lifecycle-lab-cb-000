e1aba828b7d8bdc7ec6f8185110745d4e1566fdc
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-27 09:41:41 +0000

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -11,8 +11,17 @@ class TweetWall extends React.Component {
   }
 
   // TODO: componentWillMount()
+  componentWillMount() {
+    this.setState({tweets: this.props.newTweets});
+  }
   // TODO: shouldComponentUpdate()
+  shouldComponentUpdate(nextProps) {
+    return nextProps.newTweets.length > 0;
+  }
   // TODO: componentWillReceiveProps()
+  componentWillReceiveProps(nextProps) {
+    this.setState({tweets: nextProps.newTweets.concat(this.state.tweets)});
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
